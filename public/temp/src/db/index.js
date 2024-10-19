5f05fba94cd1984285b3660c67f800695215338b
Nitish Maddheshiya <sanantkumarmaddesiya@gmail.com>
2024-10-19 11:27:13 +0530

    all setup configured

--- public/temp/src/db/index.js
+++ public/temp/src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+      const connectionInstance = await mongoose.connect("mongodb+srv://vishesh:vishesh123@cluster0.t2mqz9e.mongodb.net/Nitish")
       console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
       
     } catch (error) {
@@ -13,4 +13,4 @@ const connectDB = async () => {
     }
     
 }
- 
+ export default connectDB
